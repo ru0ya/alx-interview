bcd1ee080658ec9bf01bfeff6c51c07fb1f5e8e7
ru0ya <viktaangi02@gmail.com>
2023-05-07 21:44:58 +0300

    solved

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    """
+    function that calculates the fewest
+    number of operations needed to result in
+    n H characters in a file
+
+    Parameters: n(int)
+
+    Returns: count of operations
+    """
+    count = 1
+    while n > 0:
+        if (n % 2 == 0):
+            n = n / 2
+        else:
+            n = n - 1
+
+        count += 1
+
+    return count
