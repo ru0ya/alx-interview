55c1773cf5fe7b7923866c3969704403160b6720
ru0ya <viktaangi02@gmail.com>
2023-05-12 01:54:30 +0300

    debug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,53 @@
 #!/usr/bin/python3
-"""
-Takes an input format <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>
- if format is same: Returns total size of file size
- With every 10 lines or key interrupt
- """
+"""Script that reads stdin line by line and computes metrics"""
 
 
 import sys
-import re
-from collections import defaultdict
 
 
-input_format = (
-        r'^(\d{1,3}(?:\.\d{1,3}){3}) -\[(\d{4}-\d{2}-\d{2})]'
-        r'"GET \/projects\/(\d+) HTTP\/\d\.\d" (\d{3}) (\d+)$'
-        )
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-# metric variables
-line_count = 0
-total_file_size = 0
-status_code_count = defaultdict(int)
+def print_results(status_codes, file_size):
+    """Print statistics"""
+    print("File size: {}".format(file_size))
+    for status_code, times in sorted(status_codes.items()):
+        if times:
+            print("{}: {}".format(status_code, times))
 
 
-try:
-    # reads lines from stdin
-    for line in sys.stdin:
-        # check if it matches regex
-        match = re.match(input_format, line.strip())
-        if match:
-            # extract status code and file size from similar
-            file_size = int(match.group(5))
-            total_file_size += file_size
+def compute_metrics(lines):
+    """Compute metrics from the given lines"""
+    status_codes = {"200": 0,
+                    "301": 0,
+                    "400": 0,
+                    "401": 0,
+                    "403": 0,
+                    "404": 0,
+                    "405": 0,
+                    "500": 0
+                    }
+    file_size = 0
 
-            status_code = match.group(4)
-            status_code_count[status_code] += 1
+    for line in lines:
+        data = line.split()
+        try:
+            status_code = data[-2]
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            file_size += int(data[-1])
+        except:
+            pass
 
-            line_count += 1
-        else:
-            # skips line if it does not match expected format
-            continue
+    return status_codes, file_size
 
-        if line_count % 10 == 0:
-            # print total file size every ten lines
-            print(f'File size: {total_file_size}')
-            # print lines by status code
-            for code in status_codes:
-                if status_code_count[code] > 0:
-                    print(f'{code}: {status_code_count[code]}')
 
-except KeyboardInterrupt:
-    # Handle keyboard interruption (CTRL + C)
-    print(f'File size: {total_file_size}')
+if __name__ == '__main__':
+    lines = sys.stdin.readlines()
 
-    for code in status_codes:
-        if status_code_count[code] > 0:
-            print(f'{code}: {status_code_count[code]}')
+    try:
+        while lines:
+            chunk = lines[:10]
+            lines = lines[10:]
+            status_codes, file_size = compute_metrics(chunk)
+            print_results(status_codes, file_size)
+    except KeyboardInterrupt:
+        print_results(status_codes, file_size)
+        raise
