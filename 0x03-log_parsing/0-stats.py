491fe52af83572fd9e246df1860ac42777be3568
ru0ya <viktaangi02@gmail.com>
2023-05-12 01:56:17 +0300

    debug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics"""
 
-
 import sys
 
 
@@ -33,7 +32,9 @@ def compute_metrics(lines):
             if status_code in status_codes:
                 status_codes[status_code] += 1
             file_size += int(data[-1])
-        except:
+        except IndexError:
+            pass
+        except ValueError:
             pass
 
     return status_codes, file_size
