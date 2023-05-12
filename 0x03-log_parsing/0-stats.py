041950e10ed89895449a4d080036ad067a9f8ba9
ru0ya <viktaangi02@gmail.com>
2023-05-12 12:45:59 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,25 +7,31 @@ import re
 from collections import Counter
 from itertools import groupby
 
+
 def compute_metrics(lines):
     """Compute metrics from the given lines"""
     total_file_size = 0
     lines_by_status_code = Counter()
 
     for line in lines:
-        match = re.findall(r'^\S+ - \[\S+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line)
+        match = re.findall(
+                r'^\S+ - \[\S+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+                line)
         for status_code, file_size in match:
             total_file_size += int(file_size)
             lines_by_status_code[status_code] += 1
 
     return total_file_size, lines_by_status_code
 
+
 def print_results(total_file_size, lines_by_status_code):
     """Print final metrics"""
     print(f"File size: {total_file_size}")
+
     for code, count in lines_by_status_code.items():
         if count > 0:
             print(f"{code}: {count}")
+
 
 if __name__ == '__main__':
     lines = sys.stdin.readlines()
