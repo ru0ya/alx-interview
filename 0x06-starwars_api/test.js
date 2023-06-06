dbebf5182c3176934392d2c4c6f3ffa69a80f0f8
ru0ya <viktaangi02@gmail.com>
2023-06-06 11:34:36 +0300

    debug

--- 0x06-starwars_api/test.js
+++ 0x06-starwars_api/test.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const axios = require('axios');
+const request = require('request');
 
 const filmId = process.argv[2];
 
@@ -9,23 +8,43 @@ if (!filmId) {
   process.exit(1);
 }
 
-axios.get(`https://swapi-api.alx-tools.com/api/films/${filmId}`)
-  .then(response => {
-    const film = response.data;
-    const characterUrls = film.characters;
-    const characterPromises = characterUrls.map(url => {
-      return axios.get(url)
-        .then(response => response.data.name)
-        .catch(error => {
-          throw new Error(`Error fetching character data: ${error.message}`);
-        });
-    });
-
-    return Promise.all(characterPromises);
-  })
-  .then(characters => {
-    console.log(characters.join('\n'));
-  })
-  .catch(error => {
+request.get(`https://swapi-api.alx-tools.com/api/films/${filmId}`, (error, response, body) => {
+  if (error) {
     console.error('Error:', error.message);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', `Request failed with status code ${response.statusCode}`);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characterUrls = film.characters;
+  const characterPromises = characterUrls.map(url => {
+    return new Promise((resolve, reject) => {
+      request.get(url, (error, response, body) => {
+        if (error) {
+          reject(new Error(`Error fetching character data: ${error.message}`));
+          return;
+        }
+
+        if (response.statusCode !== 200) {
+          reject(new Error(`Request failed with status code ${response.statusCode}`));
+          return;
+        }
+
+        const character = JSON.parse(body);
+        resolve(character.name);
+      });
+    });
   });
+
+  Promise.all(characterPromises)
+    .then(characters => {
+      console.log(characters.join('\n'));
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+    });
+});
