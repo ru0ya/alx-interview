4f6ff2df514f2c2aca16d5353de53e8349844c1d
ru0ya <viktaangi02@gmail.com>
2023-06-23 13:00:30 +0300

    solved

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Function to solve the island perimeter problem
+"""
+
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """
+    Function that calculates the perimeter of a grid
+
+    Args: Grid(list) - List of lists of integer values
+                       to compute the perimeter
+
+    Returns: Perimeter(int) of a grid
+    """
+    hist = set()
+
+    def dfs(i, j):
+        if i >= len(grid) or j >= len(grid[0]) or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in hist:
+            return 0
+
+        hist.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i - 1, j)
+        return perim
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
